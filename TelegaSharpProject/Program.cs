1a4406586316fad86a08333be2f47017b873a094
likip3 <likip3@gmail.com>
2024-01-11 23:21:26 +0500

    asss

--- TelegaSharpProject/Program.cs
+++ TelegaSharpProject/Program.cs
@@ -4,19 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TelegaSharpProject.Domain.Bot;
-using TelegaSharpProject
+using TelegaSharpProject.Database;
 
 namespace TelegaSharpProject
 {
     internal class Program
     {
         private static SolverBot _solverBot;
-        private static PostgreSQL _postgre;
         private static async Task Main()
         {
-            _postgre = new PostgreSQL();
-            await _postgre.Connect();
-
             _solverBot = new SolverBot();
             await _solverBot.Start();
 
