2ad9d01d9c21891126e5dd8b67631db2e2a90c05
likip3 <likip3@gmail.com>
2024-01-19 23:14:24 +0500

    cfg

---
+++ TelegaSharpProject/Bot/Interfaces/IConfigLoader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegaSharpProject.Application.Bot.Interfaces
+{
+    internal interface IConfigLoader
+    {
+        internal string Token { get; }
+
+        internal void LoadConfigAsync(string cfgFile);
+    }
+}
