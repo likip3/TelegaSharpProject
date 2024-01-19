70945a044cfd2f38665b619c04568e746c134d4c
likip3 <likip3@gmail.com>
2024-01-19 14:20:16 +0500

    команды

--- TelegaSharpProject/Bot/Commands/CommandModule.cs
+++ TelegaSharpProject/Bot/Commands/CommandModule.cs
@@ -17,6 +17,35 @@ namespace TelegaSharpProject.Application.Bot.Commands
             SolverChat.GetSolverChat(ctx).ToTitle();
         }
 
+        [Command("/leaders", new[] { "лидеры", })]
+        public void ToLeaders(Message ctx)
+        {
+            SolverBot.botClient.SendTextMessageAsync(
+                ctx.Chat.Id,
+                MessageBuilder.GetLeaderBoard()
+            );
+        }
+
+        [Command("/mytasks")]
+        public void ToMyTasks(Message ctx)
+        {
+            SolverBot.botClient.SendTextMessageAsync(
+                ctx.Chat.Id,
+                MessageBuilder.GetMyTasks(ctx.From)
+            );
+        }
+
+        [Command("/tasks")]
+        public void ToTasks(Message ctx)
+        {
+            SolverChat.GetSolverChat(ctx).SetPage(1);
+            SolverBot.botClient.SendTextMessageAsync(
+                ctx.Chat.Id,
+                MessageBuilder.GetTasks(1),
+                replyMarkup: MessageBuilder.GetTasksMarkup()
+            );
+        }
+
         [Command("/ab")]
         public void Abobus(Message ctx)
         {
--- TelegaSharpProject/Bot/SolverChat.cs
+++ TelegaSharpProject/Bot/SolverChat.cs
@@ -40,6 +40,12 @@ namespace TelegaSharpProject.Application.Bot
             bot = SolverBot.botClient;
         }
 
+        public bool SetPage(int page)
+        {
+            if(pageNum == page || page < 1) return false;
+            pageNum = page;
+            return true;
+        }
 
         public async void ToTitle()
         {
