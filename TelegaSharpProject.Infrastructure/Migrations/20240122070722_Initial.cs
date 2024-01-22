f77128720cab79480ea0aaacd7ea6046c2714e76
likip3 <likip3@gmail.com>
2024-01-22 12:34:51 +0500

    miogrations

---
+++ TelegaSharpProject.Infrastructure/Migrations/20240122070722_Initial.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace TelegaSharpProject.Infrastructure.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    chat_id = table.Column<long>(type: "bigint", nullable: false),
+                    user_name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    registered_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    points = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "comments",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    task_id = table.Column<long>(type: "bigint", nullable: false),
+                    text = table.Column<string>(type: "text", nullable: false),
+                    ByUserId = table.Column<long>(type: "bigint", nullable: false),
+                    message_time = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_comments", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_comments_users_ByUserId",
+                        column: x => x.ByUserId,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "works",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TopicCreatorId = table.Column<long>(type: "bigint", nullable: false),
+                    task = table.Column<string>(type: "text", nullable: false),
+                    topic_start = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    mentor_end = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    AnswerId = table.Column<long>(type: "bigint", nullable: true),
+                    price = table.Column<long>(type: "bigint", nullable: false),
+                    done = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_works", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_works_comments_AnswerId",
+                        column: x => x.AnswerId,
+                        principalTable: "comments",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "FK_works_users_TopicCreatorId",
+                        column: x => x.TopicCreatorId,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_comments_ByUserId",
+                table: "comments",
+                column: "ByUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_works_AnswerId",
+                table: "works",
+                column: "AnswerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_works_TopicCreatorId",
+                table: "works",
+                column: "TopicCreatorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "works");
+
+            migrationBuilder.DropTable(
+                name: "comments");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
